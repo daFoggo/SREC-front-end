e549b745a63bdcc5059ab93767e533d21cdb7cf3
daFoggo <ntgiang141105@gmail.com>
2024-05-13 20:06:57 +0700

    change test.js

--- scripts/test.js
+++ scripts/test.js
@@ -1,73 +1,73 @@
 const questionList = {
     EXT: [
         {
-            "id": "EXT1",
+            "name": "EXT1",
             "question": "Do you find yourself being the life of the party?"
         },
         {
-            "id": "EXT2",
+            "name": "EXT2",
             "question": "Would you say that you don’t talk a lot?"
         },
         {
-            "id": "EXT3",
+            "name": "EXT3",
             "question": "Do you feel comfortable around people?"
         },
         {
-            "id": "EXT4",
+            "name": "EXT4",
             "question": "Do you prefer to keep in the background?"
         }
     ],
     EST: [
         {
-            "id": "EST1",
+            "name": "EST1",
             "question": "Do you get stressed out easily?"
         },
         {
-            "id": "EST2",
+            "name": "EST2",
             "question": "Would you say that you are relaxed most of the time?"
         },
         {
-            "id": "EST3",
+            "name": "EST3",
             "question": "Do you tend to worry about things?"
         },
         {
-            "id": "EST4",
+            "name": "EST4",
             "question": "Do you seldom feel blue?"
         }
     ],
     AGR: [
         {
-            "id": "AGR1",
+            "name": "AGR1",
             "question": "Do you feel little concern for others?"
         },
         {
-            "id": "AGR2",
+            "name": "AGR2",
             "question": "Are you interested in people?"
         },
         {
-            "id": "AGR3",
+            "name": "AGR3",
             "question": "Do you insult people?"
         },
         {
-            "id": "AGR4",
+            "name": "AGR4",
             "question": "Do you sympathize with others’ feelings?"
         }
     ],
     CSN: [
         {
-            "id": "CSN1",
+            "name": "CSN1",
             "question": "Are you always prepared?"
         },
         {
-            "id": "CSN2",
+            "name": "CSN2",
             "question": "Do you leave your belongings around?"
         },
         {
-            "id": "CSN3",
+            "name": "CSN3",
             "question": "Do you pay attention to details?"
         },
         {
-            "id": "CSN4",
+            "name": "CSN4",
             "question": "Do you make a mess of things?"
         }
     ],
@@ -100,10 +100,10 @@ document.addEventListener('DOMContentLoaded', function () {
             questionDiv.className = "question";
             questionDiv.innerHTML = `
                 <div>
-                    <span> ${question.id}. </span>
+                    <span> ${question.name}. </span>
                     ${question.question}
                 </div>
-                <select name="${question.id}" id="${question.id}">
+                <select name="${question.name}" id="${question.name}">
                     <option value="1">1: Strongly Disagree</option>
                     <option value="2">2: Disagree</option>
                     <option value="3">3: Neutral</option>
