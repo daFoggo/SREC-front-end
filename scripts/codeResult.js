bf9b9565d23431a3ac1edba1e82d50b1efc259fa
daFoggo <ntgiang141105@gmail.com>
2024-05-14 17:49:17 +0700

    ádds

--- scripts/codeResult.js
+++ scripts/codeResult.js
@@ -1,34 +1,27 @@
-
-let data = {
-    result: "Passed",
-    score: 2,
-    message1: "Congratulations!",
-    message2: "You have passed the test!"
+result = {
+    trueCase: 5,
+    falseCase: 0,
 }
-
 document.addEventListener('DOMContentLoaded', function () {
     let score = document.getElementById('score');
-    score.innerHTML = `${data.score} / 3`;
+    score.innerHTML = `${result.trueCase} / ${result.trueCase + result.falseCase}`;
 
     let resultMessage1 = document.getElementById('message1');
-    resultMessage1.innerHTML = data.message1;
+    resultMessage1.innerHTML = "Well done!";
 
     let resultMessage2 = document.getElementById('message2');
-    resultMessage2.innerHTML = data.message2;
+    resultMessage2.innerHTML = "You have passed the test.You can continue the recruitment process.";
 
-    if (data.result === "Passed") {
-        let buttonContainer = document.getElementById('buttonContainer');
-        let button = document.createElement('button');
-        button.innerHTML = "Continue the recruitment process";
-        buttonContainer.appendChild(button);
-    }
-    ;
+    let buttonContainer = document.getElementById('buttonContainer');
+    let button = document.createElement('button');
+    button.innerHTML = "Continue the recruitment process";
+    buttonContainer.appendChild(button);
 
     let ctx = document.getElementById('pieChart').getContext('2d');
     let chartData = {
         labels: ['Passed', 'Failed'],
         datasets: [{
-            data: [data.score, 3 - data.score],
+            data: [result.trueCase, result.falseCase],
             backgroundColor: ['#36A2EB', '#FF6384']
         }]
     };
